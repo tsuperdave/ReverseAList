443fbf76a38023fb315abd55804c527015a449a8
thesuperdave <davetad903@gmail.com>
2021-05-01 19:34:38 -0500

    first. Added code that passed test

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,75 @@
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.function.*;
+        import java.util.regex.*;
+        import java.util.stream.*;
+        import static java.util.stream.Collectors.joining;
+        import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> reverseArray(List<Integer> arr) {
+        // Write your code here
+        // take each element from list, put in a stack
+        // once list end is reached, pop element off stack and put into new array
+        // return new arr
+        List<Integer> reversedList = new ArrayList<>();
+        Stack<Integer> numStack = new Stack<>();
+
+        for(Integer num: arr) {
+            numStack.push(num);
+        }
+
+        int stackSize = numStack.size();
+
+        for(int i = 0; i < stackSize; i++) {
+            reversedList.add(numStack.pop());
+        }
+
+        return reversedList;
+    }
+
+}
+
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = IntStream.range(0, arrCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.reverseArray(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
